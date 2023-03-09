d4f3fe539636d63205b91eb9e2111fcd577487c1
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-03-09 23:39:13 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,41 @@
-import { FC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  //カスタムフック呼び出し
+  const { getusers, loading, users } = useAllUsers();
+  // ↓useEffectすることでuser取得を１マウントのみにできるよってパフォーマンス向上できる（N＋1問題）
+  useEffect(() => getusers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl={"https://source.unsplash.com/random"}
-          userName={"ここちゃん"}
-          fullName={"ueno CoCo"}
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <>
+          <Wrap p={{ base: 4, md: 10 }} justify="center">
+            <UserCard
+              imageUrl={"https://source.unsplash.com/random"}
+              userName={"ここちゃん"}
+              fullName={"ueno CoCo"}
+            />
+            {users.map((user) => (
+              <WrapItem key={user.id}>
+                <UserCard
+                  imageUrl={"https://source.unsplash.com/random"}
+                  userName={user.username}
+                  fullName={user.name}
+                />
+              </WrapItem>
+            ))}
+          </Wrap>
+        </>
+      )}
+    </>
   );
 });
